be79bdd2b6908442d2f54216b7b79f491124dce0
Yanoka2010 <yanasakrotenko1@gmail.com>
2024-09-27 13:54:56 +0300

    hihihaha

---
+++ bin/index.js
@@ -0,0 +1,35 @@
+//import setTribe from "../utility.js";
+import SigmaBoss from "../practice/classes/sigma.js";
+import TumbaUmba from "../practice/classes/tumba.js";
+import Tools from "../practice/classes/tools.js";
+import BattleDogs from "../practice/classes/battle.js";
+import Weapon from "../practice/classes/weapon.js";
+
+const Artem = new SigmaBoss('Artem');
+const Ivan = new TumbaUmba('Ivan');
+const Axe = new Weapon('BattleAxe');
+const Sword = new Weapon('Sword');
+const Motiga = new Tools('Motiga');
+
+Ivan.getInfo();
+Ivan.getInfoTools();
+Ivan.getInfoDogs();
+Ivan.addTools(Motiga);
+Ivan.getInfoTools();
+
+Artem.getInfo();
+Artem.getInfoWeapon();
+Artem.addWeapon(Sword);
+Artem.addWeapon(Axe);
+Artem.getInfoWeapon();
+
+const Sasha = new BattleDogs('Sasha');
+const Anna = new BattleDogs('Anna');
+Ivan.addDog(Sasha);
+Ivan.addDog(Anna);
+Ivan.getInfoDogs();
+Ivan.dogTraining(Sasha);
+Ivan.dogTraining(Anna);
+Sasha.attack(Artem);
+
+// setTribe(new TribeMember('Ivan'));
---
+++ practice/classes/battle.js
@@ -0,0 +1,38 @@
+class BattleDogs {
+  constructor(name) {
+    this.name = name;
+    this.health = 5 + Math.round(Math.random() * 25);
+    this.trainingLvl = 0;
+    this.damage = 5;
+  }
+
+  tRENEROFCKA() {
+    this.trainingLvl += 1;
+  }
+
+  takeDamage(damage) {
+    if (this.dogs.length > 0) {
+      if (this.dogs.at(0).takeDamage(damage)) {
+        this.dogs.shift();
+      }
+    } else {
+      this.health -= damage;
+      if (this.health <= 0) {
+        console.log(`${this.name} умер сорре`);
+      } else {
+        console.log(`${this.name} получил ${damage}, осталось ${this.health}`);
+      }
+    }
+  }
+
+  attack(target) {
+    console.log(`${this.name} атакует ${target.name}`);
+    if (this.trainingLvl > 0) {
+      target.takeDamage(Math.floor(this.damage * (1 + (this.trainingLvl / 10))));
+    } else {
+      target.takeDamage(this.damage);
+    }
+  }
+}
+
+export default BattleDogs;
